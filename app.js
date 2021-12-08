3b63a555ce62fd38f02d5f264e195ff4ebcf7a38
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2021-12-08 21:01:52 +0500

    homework readline

--- app.js
+++ app.js
@@ -1,10 +1,42 @@
-function showNumbers(limit) {
-    for( i = 0; i <= limit; i++  ){
-        if( i % 2 === 0 ){
-            console.log('EVEN');
-        }else{
-            console.log('ODD');
-        }
-    }
-}
-showNumbers(10);
+// function showNumbers(limit) {
+//     for( i = 0; i <= limit; i++  ){
+//         if( i % 2 === 0 ){
+//             console.log('EVEN');
+//         }else{
+//             console.log('ODD');
+//         }
+//     }
+// }
+// showNumbers(10);
+
+
+
+// const marks = [80, 80, 50];
+// console.log(calculateGrade(marks));
+
+// function calculateGrade(marks) { 
+//     natija = calculateArray(marks)   
+//     if ( natija <= 59 ) return 'F';
+//     if ( natija >= 60 && natija < 69) return 'D'; 
+//     if ( natija >= 70 && natija < 79 ) return 'C';
+//     if ( natija >= 80 && natija < 89 ) return 'B';
+//     if ( natija >= 90 && natija < 100 ) return 'A'; 
+// }
+
+// function calculateArray(array){
+//      let summa = 0    
+//     for(let i = 0; i<= marks[i]; i++){
+//         summa += marks[i]
+//     }
+//     let natija = summa/marks.length
+//     console.log(natija);
+//     return natija
+// }
+
+
+
+
+
+
+// }
+// calculateGrade(marks)
