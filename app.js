7a216497be23c6732919f66ea60b35a8c24fb01f
shakhzodbekbobobekov <shaxzodbekbobobekov@gmail.com>
2021-12-09 14:09:11 +0500

    task ready

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// todayHomeworkTask = '07.12.2021'
 // function showNumbers(limit) {
 //     for( i = 0; i <= limit; i++  ){
 //         if( i % 2 === 0 ){
@@ -8,9 +9,7 @@
 //     }
 // }
 // showNumbers(10);
-
-
-
+///////////////////////////////////////////////////////////////////
 // const marks = [80, 80, 50];
 // console.log(calculateGrade(marks));
 
@@ -32,3 +31,51 @@
 //     console.log(natija);
 //     return natija
 // }
+////////////////////////////////////////////////////////
+// todayHomeworkTask = '09.12.2021'
+
+// TASK 1
+//3 ga bo'linuvchi sonlar
+//5 ga bo'linuvchi sonlar
+
+// console.log(sum(10));
+// function sum(limit) {
+//     let summa = 0
+
+//    for( let i = 1; i <= limit; i++ ){
+//     if( (i % 3 === 0) || (i % 5 === 0) )
+//         summa += i
+//    }
+
+//    return (summa);
+// }
+
+// TASK 2
+// let summa =''
+// showStars(5)
+// function showStars(rows){
+//     for( i = 1; i<= rows; i++ ){
+//         summa += '*'
+//     console.log(summa);
+//     }
+// }
+
+// TASK 3
+// let summa =''
+// showStars(1)
+// function showStars(rows){
+//     for( i = 1; i<= rows; i++ ){
+//         summa += '*'
+//     console.log(summa);
+//     }
+// }
+
+// TASK 3
+let summa =''
+showStars(10)
+function showStars(rows){
+    for( i = 1; i<= rows; i++ ){
+        summa += '*'
+    console.log(summa);
+    }
+}
